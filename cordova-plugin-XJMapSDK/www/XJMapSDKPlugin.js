d6b7d2e9776b6453fdb48516157af94e32e00ab1
xzq2325 <673534349@qq.com>
2018-12-28 10:04:02 +0800

    js 传参

--- cordova-plugin-XJMapSDK/www/XJMapSDKPlugin.js
+++ cordova-plugin-XJMapSDK/www/XJMapSDKPlugin.js
@@ -1,9 +1,9 @@
 var exec = require('cordova/exec');
     
-exports.showMap = function ( success, error) {
-    exec(success, error, 'cordova-plugin-XJMapSDK', 'showMap');
+exports.showMap = function (mapId, success, error) {
+    exec(success, error, 'cordova-plugin-XJMapSDK', 'showMap', [mapId]);
 };
                
 exports.naviTo = function (mapId, targetName, targetId, success, error) {
-    exec(success, error, 'cordova-plugin-XJMapSDK', 'naviTo', [targetName, targetId] );
+    exec(success, error, 'cordova-plugin-XJMapSDK', 'naviTo', [mapId, targetName, targetId] );
 };
